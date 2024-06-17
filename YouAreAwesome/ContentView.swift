0dbe1a697c15baf9fa1e1c9ce3a3f8f76718e6bc
MacModeler <memcbride@mac.com>
2024-06-17 19:55:17 -0400

    Version 0.12 - Chapter 1.12

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var messageString  = ""
     @State private var imageName = ""
+    @State private var imageNumber = 0
     
     var body: some View {
         VStack {
@@ -41,13 +42,12 @@ struct ContentView: View {
                 let image0 = "image0"
                 let image1 = "image1"
                 messageString = (messageString == message1 ? message2 : message1)
-                imageName = (imageName == image0 ? image1 : image0)
-//                if messageString == message1 {
-//                    messageString = message2
-//                } else {
-//                    messageString = message1
-//                }
-            }
+                imageName = "image\(imageNumber)"
+                imageNumber += 1
+                if imageNumber > 9 {
+                    imageNumber = 0
+                }
+           }
             .buttonStyle(.borderedProminent)
             .padding()
         }
