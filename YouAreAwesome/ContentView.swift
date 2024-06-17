7f9f01c4388a4586b78c89ed892a8a21869f4829
MacModeler <memcbride@mac.com>
2024-06-17 15:02:35 -0400

    Version 0.6 - Completed chapter 1.6

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,30 +10,37 @@ import SwiftUI
 struct ContentView: View {
     @State private var messageString  = "When the Genius Bar needs help, they call you!"
     var body: some View {
-        VStack {
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundColor(.red)
-                .frame(height: 150)
-                .frame(maxWidth: .infinity)
-                  .border(.orange, width: 1)
-            HStack {
-                Button("Awesome") {
-                    messageString = "You Are Awesome"
-                }
-                .buttonStyle(.borderedProminent)
+            VStack {
+                Spacer()
                 
-                Button("Great") {
-                    messageString = "You Are Great"
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+    //                .border(.orange, width: 1)
+                
+                Spacer()
+                
+                HStack {
+                    Button("Awesome") {
+                        messageString = "You Are Awesome"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Spacer()
+                    
+                    Button("Great") {
+                        messageString = "You Are Great"
+                    }
+                    .buttonStyle(.borderedProminent)
                 }
-                .buttonStyle(.borderedProminent)
+    //            .border(.purple, width: 5)
+                .padding()
             }
-            .border(.purple, width: 5)
-        }
-            .padding()
         }
     }
     
