9f73e8802eddce44723359b81c0fe46879e8b9b8
MacModeler <memcbride@mac.com>
2024-06-16 20:35:43 -0400

    Version 0.4 - after Chapter 1.4 lesson

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,17 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString  = " "
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(.red)
+                .italic()
+            HStack {
+                Button("Awesome") {
+                    messageString = "You Are Awesome"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Button("Great") {
+                    messageString = "You Are Great"
+                }
+                .buttonStyle(.borderedProminent)
+            }
         }
-        .padding()
+            .padding()
+        }
     }
-}
-
-#Preview {
-    ContentView()
-}
+    
+    #Preview {
+        ContentView()
+    }
