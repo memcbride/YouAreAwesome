eeb2bf23aaf77d1d4ee9210412835134a9b4c011
MacModeler <memcbride@mac.com>
2024-06-18 12:51:26 -0400

    Version 0.14 - Chapter 1.14

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -43,16 +43,8 @@ struct ContentView: View {
                                 "Fabulous? That's You!",
                                 "You Are Fantastic!",
                                 "You Are Swifty"]
-                messageString = messages[messageNumber]
-                messageNumber += 1
-                if messageNumber == messages.count {
-                    messageNumber = 0
-                }
-                imageName = "image\(imageNumber)"
-                imageNumber += 1
-                if imageNumber > 9 {
-                    imageNumber = 0
-                }
+                messageString = messages[Int.random(in: 0...messages.count-1)]
+                imageName = "image\(Int.random(in: 0...9))"
            }
             .buttonStyle(.borderedProminent)
             .padding()
